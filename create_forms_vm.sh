873e50f16f50ebf3d8cc2af817eda9f92558aa24
quizforparty44 <quizforparty44@gmail.com>
2018-11-07 13:34:00 +0100

    update if vm already exists

--- create_forms_vm.sh
+++ create_forms_vm.sh
@@ -2,9 +2,21 @@
 chmod +x ./Jq/jq-linux64
 source env-vars
 
-#Create Forms Cloud VM
-oci compute instance launch --availability-domain=$region --compartment-id=$compartment_ocid --shape=$shape --display-name="formscloud" --image-id=$image_id  --subnet-id=$subnet_id --wait-for-state="RUNNING" --ssh-authorized-keys-file=$public_key_path
+#Check if the VM already exists
+export INSTANCEID=$(oci compute instance list  --compartment-id=$compartment_ocid --query "data[?\"display-name\"=='formscloud'].{Id:id}" | ./Jq/jq-linux64 .[0].Id)
+INSTANCEID=${INSTANCEID#'"'}
+INSTANCEID=${INSTANCEID::-1}
+echo "INSTANCEID = $INSTANCEID"
 
+if [ -z "$INSTANCEID" ]
+then 
+	echo "Create Forms VM"
+	#Create Forms Cloud VM
+	oci compute instance launch --availability-domain=$region --compartment-id=$compartment_ocid --shape=$shape --display-name="formscloud" --image-id=$image_id  --subnet-id=$subnet_id --wait-for-state="RUNNING" --ssh-authorized-keys-file=$public_key_path
+else
+	echo "Forms VM already Exists"
+fi
+	
 #Get the Instance Id of the Cloud VM
 export INSTANCEID=$(oci compute instance list  --compartment-id=$compartment_ocid --query "data[?\"display-name\"=='formscloud'].{Id:id}" | ./Jq/jq-linux64 .[0].Id)
 INSTANCEID=${INSTANCEID#'"'}
