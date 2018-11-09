93dfc92c2a2e15a3e8866af5eeb1f5ae7bfd2d58
quizforparty44 <quizforparty44@gmail.com>
2018-11-09 16:38:26 +0100

    error on create ddl

--- Schema/create_ddl.sql
+++ Schema/create_ddl.sql
@@ -1,4 +1,4 @@
-EXECUTE IMMEDIATE 'create tablespace summit_tbs datafile 'summit_tbs01.dbf' size 100M autoextend on'
+EXECUTE IMMEDIATE 'create tablespace summit_tbs datafile \'summit_tbs01.dbf\' size 100M autoextend on'
 EXECUTE IMMEDIATE 'CREATE USER summit IDENTIFIED BY AlphA_2014_ default tablespace summit_tbs CONTAINER=CURRENT';
 EXECUTE IMMEDIATE 'grant connect, resource to summit'
 EXECUTE IMMEDIATE 'grant create session, grant any privilege to summit'
