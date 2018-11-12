c737a8bfda4fbc989b4960082d7390090ea55f2e
quizforparty44 <quizforparty44@gmail.com>
2018-11-12 15:15:52 +0100

    update create schema

--- Schema/create_schema.sql
+++ Schema/create_schema.sql
@@ -1,5 +1,7 @@
 export MYRESULT=$(sql -noupdates -s -l @db_system_user/@db_systep_password@$db_ip_address:1521/$db_service_name @check_schema.sql)
 
+echo "MYRESULT = $MYRESULT"
+
 if [ "$MYRESULT" == "GO" ]
   sql -noupdates -s -l @db_system_user/@db_systep_password@$db_ip_address:1521/$db_service_name @create_ddl.sql
   echo "CREATE SCHEMA DONE"
